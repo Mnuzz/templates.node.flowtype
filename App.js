4a85a5e7f14282e36178c796abbca4071b5193be
Mark Nuzz <nuzz604@gmail.com>
2019-02-17 14:12:06 -0700

    Creates template for flowtype

---
+++ App.js
@@ -0,0 +1,7 @@
+// @flow
+const App2 = require('./src');
+
+const App = () => null;
+
+let b = 1;
+let c = 2;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+// @flow
+const App = () => null;
+
+export default App;
+
+function square(n: number): number {
+    return n * n;
+  }
+  
+square("2"); // Error!
+
+let a = 1;
+let b = 2;
+let c = 3;
